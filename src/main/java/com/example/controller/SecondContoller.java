83945f5e3d1d352406ecd3bf0f3f1116f09bf606
himanshurawat2807 <himanshurawat.work@gmail.com>
2020-10-01 00:20:54 +0530

    saving files

---
+++ src/main/java/com/example/controller/SecondContoller.java
@@ -0,0 +1,16 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/consumer")
+public class SecondContoller {
+
+	@GetMapping("/message")
+	public String test() {
+		return "Hello JavaInUse Called in Second Service";
+	}
+}
+
